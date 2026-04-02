95411c33d2a7cb975eac4f649918966f92d0f1a0
Sanuda <sanudasandeepa944@gmail.com>
2026-04-02 18:39:47 +0530

    this is mt first change

--- backend/product_dao.py
+++ backend/product_dao.py
@@ -1,15 +1,31 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='Sanu9988$',
+def get_all_products():
+    cnx = mysql.connector.connect(user='root', password='Sanu9988$',
                               host='127.0.0.1',
                               database='gs')
 
-cursor = cnx.cursor()
-query = "SELECT * FROM gs.products"
-cursor.execute(query)
+    cursor = cnx.cursor()
+    query = ("SELECT products.products_id,products.name,products.uom_id,products.price_per_unit,uom.uom_name "
+         "FROM products inner join uom on products.uom_id=uom.uom_id")
+    cursor.execute(query)
 
-for (products_id, name, uom_id, price_per_unit) in cursor:
-    print(products_id, name, uom_id, price_per_unit)
+    response =[]
 
-cnx.close()
+    for (products_id, name, uom_id, price_per_unit,uom_name) in cursor:
+        response.append(
+            {
+                'products_id':products_id,
+                'name':name,
+                'uom_id': uom_id,
+                'price_per_unit':price_per_unit,
+                'uom_name':uom_name
+         }
+        )
 
+    cnx.close()
+
+    return response
+
+if __name__=='__main__':
+    print(get_all_products())
